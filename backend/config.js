728b3570519dc4002d93cfbfdd3068772d434065
Hamza Sajid <hamzaqtr84@gmail.com>
2024-04-22 19:06:12 +0500

    Book App

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://root:root@book-store.j4zltu2.mongodb.net/?retryWrites=true&w=majority&appName=Book-Store';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
