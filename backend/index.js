c77de3b3a626ed581d1de9dfed51ff467cc66dd7
Hamza Sajid <hamzaqtr84@gmail.com>
2024-06-04 01:00:49 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,11 @@ const app = express();
 // Middleware for parsing request body
 app.use(express.json());
 
+app.use(express.static("./frontend/build"));
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+});
+
 // Middleware for handling CORS POLICY
 // Option 1: Allow All Origins with Default of cors(*)
 app.use(cors());
