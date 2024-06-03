67cf335f2a1a53fa5a0e5e40340830bc02e33e43
Hamza Sajid <hamzaqtr84@gmail.com>
2024-06-04 01:00:33 +0500

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,6 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  'mongodb+srv://root:root@book-store.j4zltu2.mongodb.net/?retryWrites=true&w=majority&appName=Book-Store';
+  'mongodb+srv://root:root@cluster0.dso8bzw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 // Please create a free database for yourself.
-// This database will be deleted after tutorial
